12b42cbe0c76cd7a556e71a68dfc65157007b5b4
KatAlfaro <62611561+KatAlfaro@users.noreply.github.com>
2020-05-07 18:32:05 -0600

    Ejercicio 4

---
+++ Laboratorio3/Ejercicio4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    string nombre;
+    int c;
+    float p;
+    float $;
+    cout<<"Producto: ";
+    getline(cin, nombre);
+    cout<<"Precio del producto: $";
+    cin>>p;
+    cout<<"Cantidad comprada: ";
+    cin>>c;
+    $ = p * c;
+    cout<< "\nEl total es: $"<<$;
+    return 0;}
+
+    
